942ce1ee12e21ec40a886ff4d383cfa32ca6b6a8
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2022-12-16 13:52:49 +0530

    Update PROJECT1.py

--- PROJECT1.py
+++ PROJECT1.py
@@ -1,43 +1,81 @@
-elif(game=='Hangman'):
-    print("Below is a game of hangman")
-    print("Enter the names of players")
-    player1 = input("Player1: ")
-    player2 = input("Player2: ")
-    print(player1, " enter a word for ", player2, " to guess")
-    ip = (input("Enter the word"))  # ip stores the word input by player1
-    nchar = 0
-    for char in ip:  # this for loop calculates the number of characters in the word
-        nchar = nchar + 1
-        print("The word has", nchar, " characters")
+#importing the time module
+import time
 
-        print("Guess the characters: ")  # the guessing game starts here
+#welcoming the user
+name = input("What is your name? ")
 
-        guesses = ''  # all the guesses are stored here
-        chances = 12  # any number of turns can be used here
+print ("Hello, " + name, "Time to play hangman!")
 
-    while chances > 0:
-        wchar = 0  # counts the number of times a wrong character is input
-        for char in ip:  # comparing that character with the characters in guesses
-            if char in guesses:
-                print(char)
-            else:
-                print("_")
-                wchar += 1  # for every wrong input wchar is incremented
+#wait for 1 second
+time.sleep(1)
 
-        if wchar == 0:
-            print("You Win")  # if there is no wrong input the player wins
-            print("The word is: ", ip)  # this prints the correct input
-            break
+print ("Start guessing...")
+time.sleep(0.5)
 
-    # if player has input the wrong alphabet then it will ask the player to input again
-        guess = input("guess a character: ")
-        guesses += guess  # every input character will be stored in guesses
-        if guess not in ip:  # check input with the character in word
-            chances -= 1
-            print("Wrong")  # if the character does not match , wrong is given as output
-            print("You have", + chances, 'more chances')  # prints the number of chances left for the user
-        if chances == 0:
-            print("You Loose")
-            print("The word is: ", ip)  # this prints the correct input
-else:
-    print("Invalid Try Again")
+#here we set the secret. You can select any word to play with. 
+word = ("secret")
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns = 10
+
+# Create a while loop
+
+#check if the turns are more than zero
+while turns > 0:         
+
+    # make a counter that starts with zero
+    failed = 0             
+
+    # for every character in secret_word    
+    for char in word:      
+
+    # see if the character is in the players guess
+        if char in guesses:    
+    
+        # print then out the character
+            print (char,end=""),    
+
+        else:
+    
+        # if not found, print a dash
+            print ("_",end=""),     
+       
+        # and increase the failed counter with one
+            failed += 1    
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:        
+        print ("You won")
+    # exit the script
+        break              
+
+    print
+
+    # ask the user go guess a character
+    guess = input("guess a character:") 
+
+    # set the players guess to guesses
+    guesses += guess                    
+
+    # if the guess is not found in the secret word
+    if guess not in word:  
+ 
+     # turns counter decreases with 1 (now 9)
+        turns -= 1        
+ 
+    # print wrong
+        print ("Wrong")  
+ 
+    # how many turns are left
+        print ("You have", + turns, 'more guesses' )
+ 
+    # if the turns are equal to zero
+        if turns == 0:           
+    
+        # print "You Lose"
+            print ("You Lose"  )
